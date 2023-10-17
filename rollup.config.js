54618178f58011d04a720c5b709dc6fcda2057d2
lmj01 <lmj01@users.noreply.github.com>
2023-10-17 09:15:31 +0800

    add rollup to pack

---
+++ rollup.config.js
@@ -0,0 +1,76 @@
+import commonjs from '@rollup/plugin-commonjs';
+import typescript from '@rollup/plugin-typescript';
+import {nodeResolve} from '@rollup/plugin-node-resolve';
+import replace from '@rollup/plugin-replace';
+import rawsvg from 'rollup-plugin-rawsvg'
+import postcss from 'rollup-plugin-postcss';
+
+export default [
+    {
+        input: 'quill.ts',
+        output: {            
+            file: 'dist/quill.js',
+            format: 'esm',
+        },
+        plugins: [            
+            commonjs(),
+            rawsvg(),
+            typescript({
+                outputToFilesystem: true,
+                sourceMap: true,
+                outDir: 'dist',
+            }),
+            nodeResolve({
+                // preferBuiltins:false,
+            }),
+            replace({
+                preventAssignment: true,
+                'process.env.NODE_ENV': JSON.stringify('production'),
+                // __buildDate__: () => JSON.stringify(new Date()),
+                // __buildVersion: 15,
+            })
+        ],
+    },
+    {
+        input: 'assets/core.styl',
+        output: {            
+            file: 'dist/quill.core.css',
+        },
+        plugins: [
+            postcss({
+                extract: true,
+                autoModules: false,
+                use: 'stylus',
+                sourceMap: false,
+            }),
+        ],
+    },
+    {
+        input: 'assets/bubble.styl',
+        output: {            
+            file: 'dist/quill.bubble.css',
+        },
+        plugins: [
+            postcss({
+                extract: true,
+                autoModules: false,
+                use: 'stylus',
+                sourceMap: false,
+            }),
+        ],
+    },
+    {
+        input: 'assets/snow.styl',
+        output: {            
+            file: 'dist/quill.snow.css',
+        },
+        plugins: [
+            postcss({
+                extract: true,
+                autoModules: false,
+                use: 'stylus',
+                sourceMap: false,
+            }),
+        ],
+    },
+];
